8aeb187070e6efcee5ed9d191e650d0076892af4
AntonioLara289 <156365540+AntonioLara289@users.noreply.github.com>
2024-12-05 22:34:36 -0700

    Catalogo Dinamico

--- app/src/main/java/com/fic/bunnyshopmobiletry5/catalogoAdapter/CatalogoAdapter.java
+++ app/src/main/java/com/fic/bunnyshopmobiletry5/catalogoAdapter/CatalogoAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.fic.bunnyshopmobiletry5.R;
 
 import java.util.List;
@@ -38,6 +39,15 @@ public class CatalogoAdapter extends RecyclerView.Adapter<CatalogoAdapter.Catalo
         holder.tvNombre.setText((String) producto.get("nombre"));
         //holder.tvDescripcion.setText((String) producto.get("descripcion"));
         holder.tvPrecio.setText(String.format("$%.2f", producto.get("precio")));
+
+        // Usa Glide para cargar la imagen desde una URL
+        String urlImagen = (String) producto.get("imagen"); // La clave "imagen" debe contener la URL
+        Glide.with(holder.itemView.getContext())
+                .load(urlImagen)
+                .placeholder(R.drawable.macaco_preocupado) // Imagen de carga
+                .error(R.drawable.bunny) // Imagen en caso de error
+                .into(holder.tvImagen);
+
     }
 
     @Override
