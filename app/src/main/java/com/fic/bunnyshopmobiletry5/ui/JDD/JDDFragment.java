5388429fc49da23f93fdd15ed3f39516dfa5f64d
EmmaLara289 <ea.lara21@info.uas.edu.mx>
2024-12-11 08:38:45 -0700

    Checkpoint

--- app/src/main/java/com/fic/bunnyshopmobiletry5/ui/JDD/JDDFragment.java
+++ app/src/main/java/com/fic/bunnyshopmobiletry5/ui/JDD/JDDFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,10 +17,26 @@ import com.fic.bunnyshopmobiletry5.R;
 
 public class JDDFragment extends Fragment {
 
+    private int productId;
+
     private JDDViewModel mViewModel;
 
     public static JDDFragment newInstance() {
         return new JDDFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Obtener el argumento (ID del producto)
+        if (getArguments() != null) {
+            productId = getArguments().getInt("PRODUCT_ID");
+            Log.d("ID_PRODUCTO", String.valueOf(productId));
+            return;
+        }
+
+        Log.d("SIN ARGUMENTOS", "ALgo Salio mal");
+
     }
 
     @Override
--- app/src/main/java/com/fic/bunnyshopmobiletry5/ui/ovier/OvierFragment.java
+++ app/src/main/java/com/fic/bunnyshopmobiletry5/ui/ovier/OvierFragment.java
@@ -4,15 +4,21 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fic.bunnyshopmobiletry5.R;
+
 
 public class OvierFragment extends Fragment {
 
